530e95c1d23ce3840d19b05894c114f82a30e3cf
Srinath Sridharan <srinathsr@google.com>
2016-09-08 13:47:02 -0700

    ANDROID: sched/rt: rt cpu selection integration with EAS.
    For effective interplay between RT and fair tasks. Enables sched_fifo
    for UI and Render tasks. Critical for improving user experience.
    
    bug: 24503801
    bug: 30377696
    Change-Id: I2a210c567c3f5c7edbdd7674244822f848e6d0cf
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>
    (cherry picked from commit dfe0f16b6fd3a694173c5c62cf825643eef184a3)
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1642,7 +1642,17 @@ extern unsigned int sysctl_sched_use_walt_cpu_util;
 extern unsigned int walt_ravg_window;
 extern bool walt_disabled;
 
-#endif /* CONFIG_SMP */
+static inline unsigned long task_util(struct task_struct *p)
+{
+#ifdef CONFIG_SCHED_WALT
+	if (!walt_disabled && sysctl_sched_use_walt_task_util) {
+		unsigned long demand = p->ravg.demand;
+		return (demand << 10) / walt_ravg_window;
+	}
+#endif
+	return READ_ONCE(p->se.avg.util_avg);
+}
+#endif
 
 static inline void sched_rt_avg_update(struct rq *rq, u64 rt_delta)
 {
