1c8f73fa648e31cba203153cb422e21de281e9e7
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-12-12 16:44:10 -0800

    mm: add vfree_atomic()
    We are going to use sleeping lock for freeing vmap.  However some
    vfree() users want to free memory from atomic (but not from interrupt)
    context.  For this we add vfree_atomic() - deferred variation of vfree()
    which can be used in any atomic context (except NMIs).
    
    [akpm@linux-foundation.org: tweak comment grammar]
    [aryabinin@virtuozzo.com: use raw_cpu_ptr() instead of this_cpu_ptr()]
      Link: http://lkml.kernel.org/r/1481553981-3856-1-git-send-email-aryabinin@virtuozzo.com
    Link: http://lkml.kernel.org/r/1479474236-4139-5-git-send-email-hch@lst.de
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Jisheng Zhang <jszhang@marvell.com>
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: John Dias <joaodias@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Git-commit: bf22e37a641327e34681b7b6959d9646e3886770
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Change-Id: I5f67e939774da6e811f3a5180a6b0f5d31fbe32b
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -2306,14 +2306,6 @@ void vfree_atomic(const void *addr)
 	__vfree_deferred(addr);
 }
 
-static void __vfree(const void *addr)
-{
-	if (unlikely(in_interrupt()))
-		__vfree_deferred(addr);
-	else
-		__vunmap(addr, 1);
-}
-
 /**
  *	vfree  -  release memory allocated by vmalloc()
  *	@addr:		memory base address
@@ -2338,8 +2330,10 @@ void vfree(const void *addr)
 
 	if (!addr)
 		return;
-
-	__vfree(addr);
+	if (unlikely(in_interrupt()))
+		__vfree_deferred(addr);
+	else
+		__vunmap(addr, 1);
 }
 EXPORT_SYMBOL(vfree);
 
