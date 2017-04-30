d324a92582aae2b3052a4e8cbf154888c853809b
Andrew Perepechko <andrew.perepechko@seagate.com>
2017-04-30 00:46:35 -0400

    ext4: preload block group descriptors
    With enabled meta_bg option block group descriptors
    reading IO is not sequential and requires optimization.
    
    Signed-off-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3854,6 +3854,12 @@ static int ext4_fill_super(struct super_block *sb, void *data, int silent)
 
 	bgl_lock_init(sbi->s_blockgroup_lock);
 
+	/* Pre-read the descriptors into the buffer cache */
+	for (i = 0; i < db_count; i++) {
+		block = descriptor_loc(sb, logical_sb_block, i);
+		sb_breadahead(sb, block);
+	}
+
 	for (i = 0; i < db_count; i++) {
 		struct buffer_head *bh;
 
