800e86742d374bffd3b5a795f19a8f39f64d0c9a
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-18 22:07:57 -0700

    devfreq_boost: Introduce devfreq boost driver
    This driver boosts enumerated devfreq devices upon input, and allows for
    boosting specific devfreq devices on other custom events. The boost
    frequencies for this driver should be set so that frame drops are
    near-zero at the boosted frequencies and power consumption is minimized
    at said frequencies. The goal of this driver is to provide an interface
    to achieve optimal device performance by requesting boosts on key
    events, such as when a frame is ready to rendered to the display.
    
    Currently, support is only present for boosting the cpubw devfreq
    device, but the driver is structured in a way that makes it easy to add
    support for new boostable devfreq devices in the future.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- include/linux/devfreq.h
+++ include/linux/devfreq.h
@@ -250,6 +250,9 @@ struct devfreq_simple_ondemand_data {
 };
 #endif
 
+/* Caution: devfreq->lock must be locked before calling update_devfreq */
+extern int update_devfreq(struct devfreq *devfreq);
+
 #else /* !CONFIG_PM_DEVFREQ */
 static inline struct devfreq *devfreq_add_device(struct device *dev,
 					  struct devfreq_dev_profile *profile,
@@ -317,6 +320,11 @@ static inline void devm_devfreq_unregister_opp_notifier(struct device *dev,
 }
 
 static inline int devfreq_update_stats(struct devfreq *df)
+{
+	return -EINVAL;
+}
+
+static inline int update_devfreq(struct devfreq *devfreq)
 {
 	return -EINVAL;
 }
