e5f9c2bfc59fd580929ee87883659a5a31ee1bfa
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-04 17:42:08 -0700

    mm: vmpressure: Ignore allocation orders above PAGE_ALLOC_COSTLY_ORDER
    PAGE_ALLOC_COSTLY_ORDER allocations can cause vmpressure to incorrectly
    think that memory pressure is high, when it's really just that the
    allocation's high order is difficult to satisfy. When this rare scenario
    occurs, ignore the input to vmpressure to avoid sending out a spurious
    high-pressure signal.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2598,6 +2598,10 @@ static bool shrink_zone(struct zone *zone, struct scan_control *sc,
 			reclaim_state->reclaimed_slab = 0;
 		}
 
+		vmpressure(sc->gfp_mask, sc->target_mem_cgroup,
+			   sc->nr_scanned - nr_scanned,
+			   sc->nr_reclaimed - nr_reclaimed, sc->order);
+
 		if (sc->nr_reclaimed - nr_reclaimed)
 			reclaimable = true;
 
