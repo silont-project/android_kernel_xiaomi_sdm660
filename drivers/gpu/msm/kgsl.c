dae29d92764ab1c62506e0ead5fc012b7c3886f1
Reinazhard <muh.alfarozy@gmail.com>
2020-08-22 04:45:02 +0700

    Revert "msm: kgsl: correct per process stat maintenance"
    This reverts commit df3cb0cfe2b01a3e67b766d1d71cfcc56d6c0df4.

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -300,10 +300,6 @@ kgsl_mem_entry_destroy(struct kref *kref)
 	/* pull out the memtype before the flags get cleared */
 	memtype = kgsl_memdesc_usermem_type(&entry->memdesc);
 
-	if (!(entry->memdesc.flags & KGSL_MEMFLAGS_SPARSE_VIRT))
-		kgsl_process_sub_stats(entry->priv, memtype,
-			entry->memdesc.size);
-
 	/* Detach from process list */
 	kgsl_mem_entry_detach_process(entry);
 
@@ -462,10 +458,7 @@ static void kgsl_mem_entry_detach_process(struct kgsl_mem_entry *entry)
 	entry->id = 0;
 
 	type = kgsl_memdesc_usermem_type(&entry->memdesc);
-
-	if (type != KGSL_MEM_ENTRY_ION)
-		entry->priv->gpumem_mapped -= entry->memdesc.mapsize;
-
+	entry->priv->stats[type].cur -= entry->memdesc.size;
 	spin_unlock(&entry->priv->mem_lock);
 
 	kgsl_mmu_put_gpuaddr(&entry->memdesc);
@@ -3409,10 +3402,6 @@ long kgsl_ioctl_sparse_phys_alloc(struct kgsl_device_private *dev_priv,
 
 	param->id = entry->id;
 	param->flags = entry->memdesc.flags;
-
-	kgsl_process_add_stats(process,
-			kgsl_memdesc_usermem_type(&entry->memdesc),
-			entry->memdesc.size);
 
 	trace_sparse_phys_alloc(entry->id, param->size, param->pagesize);
 	kgsl_mem_entry_commit_process(entry);
