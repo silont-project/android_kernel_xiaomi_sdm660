fb0caa4b2d763dab579a60ad456a2a56f4c86d25
Reinazhard <muh.alfarozy@gmail.com>
2020-10-04 00:24:53 +0700

    fbdev: nuke undefined reference

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -1726,9 +1726,6 @@ static int mdss_fb_create_sysfs(struct msm_fb_data_type *mfd)
 #endif
 
 	rc = sysfs_create_group(&mfd->fbi->dev->kobj, &mdss_fb_attr_group);
-	if (rc)
-		pr_err("sysfs group creation failed, rc=%d\n", rc);
-	return mdss_livedisplay_create_sysfs(mfd);
 }
 
 static void mdss_fb_remove_sysfs(struct msm_fb_data_type *mfd)
