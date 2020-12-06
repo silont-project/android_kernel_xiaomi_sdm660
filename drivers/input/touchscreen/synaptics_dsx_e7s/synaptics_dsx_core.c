bc91335cf7c1dc1065d5047ed1ba25f4dbfc2d28
Reinazhard <muh.alfarozy@gmail.com>
2020-12-07 00:31:12 +0700

    Revert "drivers: input: touchscreen: Allow using dt2w once enabled from device sysfs node"
    This reverts commit 8f696b8e4b9174228d4272371a1657a073ed4a1a.

--- drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
+++ drivers/input/touchscreen/synaptics_dsx_e7s/synaptics_dsx_core.c
@@ -958,12 +958,10 @@ static ssize_t synaptics_rmi4_wake_gesture_store(struct device *dev,
 	if (sscanf(buf, "%u", &input) != 1)
 		return -EINVAL;
 
-	input_event(rmi4_data->input_dev, EV_SYN, SYN_CONFIG, input ? WAKEUP_ON : WAKEUP_OFF);
+	input = input > 0 ? 1 : 0;
 
-	if(synaptics_gesture_func_on)
-		input = input > 0 ? 1 : 0;
-	else
-		input = 0;
+	if (rmi4_data->suspend && input != rmi4_data->enable_wakeup_gesture)
+		gesture_delay = true;
 
 	if (rmi4_data->f11_wakeup_gesture || rmi4_data->f12_wakeup_gesture)
 		synaptics_gesture_func_on = rmi4_data->enable_wakeup_gesture = input;
