dc8a65553b5affb824457396e0f5e8d1554335bf
Reinazhard <muh.alfarozy@gmail.com>
2021-03-05 17:53:17 +0000

    cpufreq_schedutil: reapply 0d0620989a687197598fdb1a200492fff513e3f7
    current value messed up the freq stats
    
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -779,8 +779,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->up_rate_limit_us = 500;
-	tunables->down_rate_limit_us = 20000;
+	tunables->up_rate_limit_us = 20000;
+	tunables->down_rate_limit_us = 500;
 
 	tunables->iowait_boost_enable = true;
 	policy->governor_data = sg_policy;
