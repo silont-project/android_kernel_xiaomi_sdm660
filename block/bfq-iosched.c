2f678407a69399fbc99d776e0fbba67e64776576
Paolo Valente <paolo.valente@linaro.org>
2021-03-23 05:32:46 +0000

    block, bfq: remove case of redirected bic from insert_request
    Before commit 18e5a57 ("block, bfq: postpone rq preparation to
    insert or merge"), the destination queue for a request was chosen by a
    different hook than the one that then inserted the request. So, between
    the execution of the two hooks, the bic of the process generating the
    request could happen to be redirected to a different bfq_queue. As a
    consequence, the destination bfq_queue stored in the request could be
    wrong. Such an event does not need to ba handled any longer.
    
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -4332,8 +4332,6 @@ static void bfq_insert_request(struct request_queue *q, struct request *rq)
 	if (!in_interrupt()) {
 		new_bfqq = bfq_setup_cooperator(bfqd, bfqq, rq, true);
 		if (new_bfqq) {
-			if (bic_to_bfqq(RQ_BIC(rq), 1) != bfqq)
-				new_bfqq = bic_to_bfqq(RQ_BIC(rq), 1);
 			/*
 			 * Release the request's reference to the old bfqq
 			 * and make sure one is taken to the shared queue.
