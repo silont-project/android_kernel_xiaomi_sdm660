83d240c5a4060393917842e06ff2eeedafc80db6
Reinazhard <muh.alfarozy@gmail.com>
2021-04-24 00:04:12 +0700

    Revert "mm: separate out the invocation of lowmemorykiller shrinker"
    * This feature has been dropped
    
    This reverts commit e8d903009b546d6cf7844495e3b734098922de26.

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -352,8 +352,7 @@ static inline struct task_struct *pick_last_task(void)
 static struct shrinker lowmem_shrinker = {
 	.scan_objects = lowmem_scan,
 	.count_objects = lowmem_count,
-	.seeks = DEFAULT_SEEKS * 16,
-	.flags = SHRINKER_LMK
+	.seeks = DEFAULT_SEEKS * 16
 };
 
 static int __init lowmem_init(void)
