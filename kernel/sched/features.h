35fa57d7dc0757ac6f9c35bf011c4cc1fd135290
Sultan Alsawaf <sultan@kerneltoast.com>
2021-05-06 00:10:36 -0700

    BACKPORT: sched: Resolve sched_feat() at compile time to improve code optimization
    Keeping the compiler in the dark about which sched_feat() toggles are
    set reduces its ability to optimize scheduler code, which results in a
    measurably significant performance loss since the affected code is very
    hot. Turning sched_feat() definitions into macros that the compiler can
    resolve results in smaller and faster code.
    
    The sched_feat() definitions are converted to macros with this sed:
    sed -i -E \
    	-e 's/SCHED_FEAT\((.*),(.*)\)/#define SCHED_FEAT_\1\2/' \
    	-e 's/true/1/' \
    	-e 's/false/0/' \
    	kernel/sched/features.h
    
    The sched_feat() macros are named SCHED_FEAT_*. With this done, we can
    now optimize try-to-wake-up code when SCHED_FEAT_TTWU_QUEUE is disabled,
    which results in a CPU usage reduction of a few percent inside
    try_to_wake_up().
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [Kaz205: Backport to v4.4]
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -3,53 +3,53 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, true)
+#define SCHED_FEAT_GENTLE_FAIR_SLEEPERS 1
 
 /*
  * Place new tasks ahead so that they do not starve already running
  * tasks
  */
-SCHED_FEAT(START_DEBIT, true)
+#define SCHED_FEAT_START_DEBIT 1
 
 /*
  * Prefer to schedule the task we woke last (assuming it failed
  * wakeup-preemption), since its likely going to consume data we
  * touched, increases cache locality.
  */
-SCHED_FEAT(NEXT_BUDDY, false)
+#define SCHED_FEAT_NEXT_BUDDY 0
 
 /*
  * Prefer to schedule the task that ran last (when we did
  * wake-preempt) as that likely will touch the same data, increases
  * cache locality.
  */
-SCHED_FEAT(LAST_BUDDY, true)
+#define SCHED_FEAT_LAST_BUDDY 1
 
 /*
  * Consider buddies to be cache hot, decreases the likelyness of a
  * cache buddy being migrated away, increases cache locality.
  */
-SCHED_FEAT(CACHE_HOT_BUDDY, true)
+#define SCHED_FEAT_CACHE_HOT_BUDDY 1
 
 /*
  * Allow wakeup-time preemption of the current task:
  */
-SCHED_FEAT(WAKEUP_PREEMPTION, true)
+#define SCHED_FEAT_WAKEUP_PREEMPTION 1
 
-SCHED_FEAT(HRTICK, false)
-SCHED_FEAT(DOUBLE_TICK, false)
-SCHED_FEAT(LB_BIAS, true)
+#define SCHED_FEAT_HRTICK 0
+#define SCHED_FEAT_DOUBLE_TICK 0
+#define SCHED_FEAT_LB_BIAS 1
 
 /*
  * Decrement CPU capacity based on time not spent running tasks
  */
-SCHED_FEAT(NONTASK_CAPACITY, true)
+#define SCHED_FEAT_NONTASK_CAPACITY 1
 
 /*
  * Queue remote wakeups on the target CPU and process them
  * using the scheduler IPI. Reduces rq->lock contention/bounces.
  */
-SCHED_FEAT(TTWU_QUEUE, true)
+#define SCHED_FEAT_TTWU_QUEUE 1
 
 #ifdef HAVE_RT_PUSH_IPI
 /*
@@ -61,27 +61,29 @@ SCHED_FEAT(TTWU_QUEUE, true)
  * IPI to that CPU and let that CPU push the RT task to where
  * it should go may be a better scenario.
  */
-SCHED_FEAT(RT_PUSH_IPI, true)
+#define SCHED_FEAT_RT_PUSH_IPI 1
+#else
+#define SCHED_FEAT_RT_PUSH_IPI 0
 #endif
 
-SCHED_FEAT(FORCE_SD_OVERLAP, false)
-SCHED_FEAT(RT_RUNTIME_SHARE, false)
-SCHED_FEAT(LB_MIN, false)
-SCHED_FEAT(ATTACH_AGE_LOAD, true)
+#define SCHED_FEAT_FORCE_SD_OVERLAP 0
+#define SCHED_FEAT_RT_RUNTIME_SHARE 0
+#define SCHED_FEAT_LB_MIN 0
+#define SCHED_FEAT_ATTACH_AGE_LOAD 1
 
 /*
  * UtilEstimation. Use estimated CPU utilization.
  */
-SCHED_FEAT(UTIL_EST, true)
+#define SCHED_FEAT_UTIL_EST 1
 
 /*
  * Energy aware scheduling. Use platform energy model to guide scheduling
  * decisions optimizing for energy efficiency.
  */
 #ifdef CONFIG_DEFAULT_USE_ENERGY_AWARE
-SCHED_FEAT(ENERGY_AWARE, true)
+#define SCHED_FEAT_ENERGY_AWARE 1
 #else
-SCHED_FEAT(ENERGY_AWARE, false)
+#define SCHED_FEAT_ENERGY_AWARE 0
 #endif
 
 /*
@@ -91,7 +93,7 @@ SCHED_FEAT(ENERGY_AWARE, false)
  *   the EAS path for wakeup task placement. Otherwise, put
  *   those tasks through the mainline slow path.
  */
-SCHED_FEAT(EAS_PREFER_IDLE, true)
+#define SCHED_FEAT_EAS_PREFER_IDLE 1
 
 /*
  * Minimum capacity capping. Keep track of minimum capacity factor when
@@ -99,18 +101,18 @@ SCHED_FEAT(EAS_PREFER_IDLE, true)
  * If enabled, this can be used to inform the scheduler about capacity
  * restrictions.
  */
-SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
+#define SCHED_FEAT_MIN_CAPACITY_CAPPING 0
 
 /*
  * Enforce the priority of candidates selected by find_best_target()
  * ON: If the target CPU saves any energy, use that.
  * OFF: Use whichever of target or backup saves most.
  */
-SCHED_FEAT(FBT_STRICT_ORDER, false)
+#define SCHED_FEAT_FBT_STRICT_ORDER 0
 
 /*
  * Inflate the effective utilization of SchedTune-boosted tasks, which
  * generally leads to usage of higher frequencies.
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
-SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
+#define SCHED_FEAT_SCHEDTUNE_BOOST_UTIL 0
