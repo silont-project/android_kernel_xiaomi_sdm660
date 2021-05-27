ac453ccb757aa771ddbb91956fb89389b30bf573
Reinazhard <reinazhard@gmail.com>
2021-05-27 01:16:42 +0000

    Revert "kernel: Boost DDR bus for a short amount of time when zygote forks"
    This reverts commit ceebaab26be47c16c5d8ce291387f9b89d2f275e.

--- kernel/fork.c
+++ kernel/fork.c
@@ -80,7 +80,6 @@
 #include <linux/kcov.h>
 #include <linux/cpufreq.h>
 #include <linux/simple_lmk.h>
-#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1816,10 +1815,6 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current))
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
