c357294a38623ee870f9af7506a64066ec9aadd8
Reinazhard <reinazhard@gmail.com>
2021-06-02 01:43:25 +0000

    Fix build after cpufreq upstream
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2652,8 +2652,8 @@ int trigger_cpufreq_underclock(void)
 	pr_info("Triggered cpu underclock\n");
 	for_each_policy(policy) {
 		cpufreq_underclock_set(cpufreq_underclock_check_cluster(policy->cpu),policy,true);
-		__cpufreq_governor(policy, CPUFREQ_GOV_STOP);
-		__cpufreq_governor(policy, CPUFREQ_GOV_START);
+		cpufreq_governor(policy, CPUFREQ_GOV_STOP);
+		cpufreq_governor(policy, CPUFREQ_GOV_START);
 		
 	}
 	underclocked=true;
@@ -2666,8 +2666,8 @@ int resume_cpufreq_underclock(void)
 	pr_info("Resumed cpu underclock\n");
 	for_each_policy(policy) {
 		cpufreq_underclock_set(cpufreq_underclock_check_cluster(policy->cpu),policy,false);
-		__cpufreq_governor(policy, CPUFREQ_GOV_STOP);
-		__cpufreq_governor(policy, CPUFREQ_GOV_START);
+		cpufreq_governor(policy, CPUFREQ_GOV_STOP);
+		cpufreq_governor(policy, CPUFREQ_GOV_START);
 		
 	}
 	underclocked=false;
