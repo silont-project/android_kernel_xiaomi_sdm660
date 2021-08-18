f3d6752d49758a48f20bbee789417e0612161772
Reinazhard <reinazhard@gmail.com>
2021-08-18 10:13:05 +0700

    sched/tune: set top-app to 1
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -792,7 +792,7 @@ struct st_data {
 static void write_default_values(struct cgroup_subsys_state *css)
 {
 	static struct st_data st_targets[] = {
-		{ "top-app",	2, 1 },
+		{ "top-app",	1, 1 },
 		{ "foreground",	0, 1 },
 		{ "rt",		1, 1 }
 	};
